3d85ccab3fc7cf011574cd14e88a70e00f73f7cd
carrotbat410 <carrotbat410@gmail.com>
2025-02-09 18:33:51 +0900

    spring boot initializar 및 db연결 (spring data jpa는 db연결안하니 서버실행이 안되더라)

---
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/LolTeamBalanceToolApplication.java
@@ -0,0 +1,13 @@
+package com.carrotbat410.lol_team_balance_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolTeamBalanceToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolTeamBalanceToolApplication.class, args);
+	}
+
+}
