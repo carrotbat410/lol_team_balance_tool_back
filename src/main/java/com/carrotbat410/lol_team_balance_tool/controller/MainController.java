6da3432bec3bfb3876b95b9634dcbce46ed2a848
carrotbat410 <carrotbat410@gmail.com>
2025-04-20 16:36:02 +0900

    /컨트롤러에 테스트용 getName, getAuthority하는 방법 구현해봄

--- src/main/java/com/carrotbat410/lol_team_balance_tool/controller/MainController.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.carrotbat410.lol_team_balance_tool.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 
 @RestController
@@ -10,6 +16,18 @@ public class MainController {
 
     @GetMapping("/")
     public String main() {
-        return "hi";
+        //get username하는 방법
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //get authority하는 방법
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+
+        return "Main Controller : "+name + " | role : " + role;
     }
 }
