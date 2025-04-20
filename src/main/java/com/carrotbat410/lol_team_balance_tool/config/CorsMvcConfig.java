7187b9404244242c8a71b1e335ca0d3d2492f2b5
carrotbat410 <carrotbat410@gmail.com>
2025-04-20 17:03:04 +0900

    REST API(=Srping MVC레벨)에 대한 CORS 설정

---
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/config/CorsMvcConfig.java
@@ -0,0 +1,18 @@
+package com.carrotbat410.lol_team_balance_tool.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//* Spring MVC 레벨에서 CORS 설정
+//* 즉,@RestController로 만든 API에 대해서만 동작함.
+//* Spring Security가 적용되지 않은 컨트롤러를 위해 추가해줌.
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
