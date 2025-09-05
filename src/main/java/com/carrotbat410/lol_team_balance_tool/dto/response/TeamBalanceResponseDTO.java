50ca1046af27cac6b645a4091f969450d4e7a697
carrotbat410 <carrotbat410@gmail.com>
2025-09-05 21:36:51 +0900

    TeamBalanceResponseDTO 작성

--- src/main/java/com/carrotbat410/lol_team_balance_tool/dto/response/TeamBalanceResponseDTO.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/dto/response/TeamBalanceResponseDTO.java
@@ -7,18 +7,26 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+@Getter
 public class TeamBalanceResponseDTO {
 
-    private final List<SummonerDTO> finalTeam1List;
-    private final List<SummonerDTO> finalTeam2List;
-    private final int finalTeam1AvgMmr;
-    private final int finalTeam2AvgMmr;
-    private final Tier finalTeam1AvgTier;
-    private final int finalTeam1AvgRank;
-    private final Tier finalTeam2AvgTier;
-    private final int finalTeam2AvgRank;
+    private final TeamBalanceTeamInfo team1Info;
+    private final TeamBalanceTeamInfo team2Info;
     private final int avgMmrDiff;
 
-
+    public TeamBalanceResponseDTO(TeamBalanceTeamInfo team1Info, TeamBalanceTeamInfo team2Info) {
+        this.team1Info = team1Info;
+        this.team2Info = team2Info;
+        this.avgMmrDiff = Math.abs(team1Info.getFinalTeamAvgMmr() - team2Info.getFinalTeamAvgMmr());
+    }
 }
+
+@Getter
+@RequiredArgsConstructor
+class TeamBalanceTeamInfo {
+    private final List<SummonerDTO> finalTeamList;
+    private final int finalTeamAvgMmr;
+    private final Tier finalTeamAvgTier;
+    private final int finalTeamAvgRank;
+}
+
