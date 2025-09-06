28f7e59570dc310ee3604dc6dfd70c1c4f8b92e4
carrotbat410 <carrotbat410@gmail.com>
2025-09-06 20:21:35 +0900

    CORS 허용 추가

--- src/main/java/com/carrotbat410/lol_team_balance_tool/config/CorsMvcConfig.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/config/CorsMvcConfig.java
@@ -13,6 +13,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000", "http://172.30.1.52:3000");
     }
 }
