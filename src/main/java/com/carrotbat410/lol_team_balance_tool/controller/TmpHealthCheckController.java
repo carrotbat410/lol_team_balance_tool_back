047cf427b445da517b23181b6036c8da3abaa35b
carrotbat410 <carrotbat410@gmail.com>
2025-09-13 13:50:22 +0900

    헬스체크 api에 port번호 찍히도록 로그남김

--- src/main/java/com/carrotbat410/lol_team_balance_tool/controller/TmpHealthCheckController.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/controller/TmpHealthCheckController.java
@@ -3,17 +3,26 @@ package com.carrotbat410.lol_team_balance_tool.controller;
 import com.carrotbat410.lol_team_balance_tool.dto.response.SuccessResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @Tag(name = "TmpHealthCheckController", description = "임시 헬스체크 컨트롤러")
 public class TmpHealthCheckController {
+
+    private final Environment env;
 
     @GetMapping("/tmpHealthCheck")
     @Operation(summary = "임시 헬스체크", description = "임시 헬스체크용 컨트롤러입니다.")
     public SuccessResponseDTO<Void> tmpHealthCheck() {
+        String port = env.getProperty("local.server.port");
+        log.info("Health check on port: {}", port);
 
-        return new SuccessResponseDTO<>("tmp health check ok");
+        return new SuccessResponseDTO<>("tmp health check ok on port " + port);
     }
 }
