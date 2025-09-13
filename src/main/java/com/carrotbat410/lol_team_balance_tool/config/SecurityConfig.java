351638721fb7e54eed202bde0ba36b5c9ecbf23d
carrotbat410 <carrotbat410@gmail.com>
2025-09-14 08:02:30 +0900

    프론트엔드 cors 설정

--- src/main/java/com/carrotbat410/lol_team_balance_tool/config/SecurityConfig.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://172.30.1.52:3000"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://43.201.73.164"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
