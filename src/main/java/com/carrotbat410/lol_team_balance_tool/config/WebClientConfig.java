d1a015ed4228abdaba1490ac74a64b950ebd8165
carrotbat410 <carrotbat410@gmail.com>
2025-09-14 15:03:05 +0900

    base-url 하이픈 없애려고 baseurl로 변경함(github action secrets에 하이픈 등록안됨)

--- src/main/java/com/carrotbat410/lol_team_balance_tool/config/WebClientConfig.java
+++ src/main/java/com/carrotbat410/lol_team_balance_tool/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.carrotbat410.lol_team_balance_tool.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,24 +9,24 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${riot.api.base-url.kr}")
-    private String riotApiBaseUrlKr;
+    @Value("${riot.api.baseurl.kr}")
+    private String riotApiBaseurlKr;
 
-    @Value("${riot.api.base-url.asia}")
-    private String riotApiBaseUrlAsia;
+    @Value("${riot.api.baseurl.asia}")
+    private String riotApiBaseurlAsia;
 
     @Primary
     @Bean("krWebClient")
     public WebClient krWebClient(WebClient.Builder builder) {
         return builder
-                .baseUrl(riotApiBaseUrlKr)
+                .baseUrl(riotApiBaseurlKr)
                 .build();
     }
 
     @Bean("asiaWebClient")
     public WebClient asiaWebClient(WebClient.Builder builder) {
         return builder
-                .baseUrl(riotApiBaseUrlAsia)
+                .baseUrl(riotApiBaseurlAsia)
                 .build();
     }
 }
